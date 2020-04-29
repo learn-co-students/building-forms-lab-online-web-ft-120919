b1a92cb3cd9a077a1a1f05709380d76e3fcf5535
github username <meliajm@gmail.com>
2020-04-29 11:03:57 -0700

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,51 @@
 // Add BandInput component
+
 import React, { Component } from 'react'
+import { addBand } from '../actions'
+import { connect } from 'react-redux';
 
 class BandInput extends Component {
+
+  state = {
+    name: '',
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault()
+    const band = {...this.state}
+    console.log('state', this.state)
+    console.log('band', band)
+    this.props.addBand(band)
+    // console.log(this.props.addBand(band))
+    this.setState({
+      name: ''
+    })
+  }
+
   render() {
     return(
       <div>
         Band Input
+        <form onSubmit={(event)=>this.handleOnSubmit(event)}>
+
+          <input 
+          type="text" 
+          onChange={(event) => this.handleOnChange(event)}
+          name="name"
+          value={this.state.name}
+          />
+          <input type="submit"/>
+        </form>
       </div>
     )
   }
 }
 
 export default BandInput
+// export default connect(null, { addBand })(BandInput);
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+// import { addBand } from '../actions/index'
+import BandCard from '../components/BandCard'
+import BandInput from '../components/BandInput'
 
 class BandsContainer extends Component {
   render() {
+
+
+
+
     return(
       <div>
+        <BandInput addBand={this.props.addBand} />
         BandsContainer
+        <BandCard bands={this.props.bands} />
+        {/* {bands.map(band => <BandCard band={band}/>)} */}
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapDispatchToProps = dispatch => {
+  return (
+    {addBand: band => dispatch({type: 'ADD_BAND', band})}
+  )
+}
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addRestaurant: (newRestaurant) => {
+//       dispatch(addRestaurant(newRestaurant))
+//     }
+//   };
+// };
+
+const mapStateToProps = ({bands})=> ({
+      bands
+})
+
+// const mapStateToProps = state => {
+//   return {
+//       bands: state.bands
+//   }
+// }
+
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
+// export default BandsContainer
