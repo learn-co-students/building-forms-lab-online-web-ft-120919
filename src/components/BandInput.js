3b2264f27c9a551817c82c750b8a992b69401508
rick <github email address>
2020-05-10 17:54:42 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,10 +2,23 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  constructor(props){
+    super(props)
+  }
+  state={name:''}
+  handlChange(e){
+    this.setState({
+      name: e.target.value
+    })
+  }
   render() {
+    console.log(this.props)
     return(
       <div>
-        Band Input
+        <form onSubmit={()=>this.props.addBand(this.state)}>
+          <input type="text" name="name" value={this.state.name}onChange={(e)=>this.handlChange(e)}/>
+          <input type="submit" value=""/>
+        </form>
       </div>
     )
   }
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
+import BandInput from '../components/BandInput'
+import { connect } from 'react-redux'
+// import {addBand} from '../reducers/actions/band'
 
 class BandsContainer extends Component {
+  addBand(){
+    debugger
+  }
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.addBand}/>
       </div>
     )
   }
 }
 
-export default BandsContainer
+export default connect(null)(BandsContainer)
